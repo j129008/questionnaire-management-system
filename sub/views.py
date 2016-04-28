fa91c634796d4625dfbcab3df0f3ebe6ff263041
j129008 <j129008@gmail.com>
2016-04-28 16:28:29 +0800

    load lv1 list

--- sub/views.py
+++ sub/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lv1
 
 def subject(request):
-    return render(request, 'subject.html')
+    lv1_list = Lv1.objects.all()
+    return render(request, 'subject.html', {
+        'lv1_list': lv1_list,
+        })
