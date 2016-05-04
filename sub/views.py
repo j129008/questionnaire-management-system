37bb3c80d92880d771116b65f5f86366ea61478e
j129008 <j129008@gmail.com>
2016-05-04 21:45:43 +0800

    selected table

--- sub/views.py
+++ sub/views.py
@@ -1,9 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
-from .models import Lv1
+# from .models import subject
 
-def subject(request):
-    lv1_list = Lv1.objects.all()
-    return render(request, 'subject.html', {
-        'lv1_list': lv1_list,
+subject_list = {
+    '學生':{
+        '基本資料': ['性別', '宗教'],
+        '生活經驗': ['生活事件']
+    },
+    '父母':{
+        '成長經驗': ['祖父母背景']
+    },
+    '家庭':{},
+    '同儕關係':{},
+    '學校生活':{},
+    '社區':{},
+    '研究方法':{},
+    '社會資本':{},
+    '文化資本':{}
+}
+
+
+
+def sub(request):
+    try:
+        level2_list = subject_list[request.GET['level1']]
+    except:
+        level2_list = ''
+
+    try:
+        level3_list = subject_list[request.GET['level1']][request.GET['level2']]
+    except:
+        level3_list = ''
+
+    try:
+        selected1 = request.GET['level1']
+    except:
+        selected1 = ''
+
+    try:
+        selected2 = request.GET['level2']
+    except:
+        selected2 = ''
+
+    try:
+        selected3 = request.GET['level3']
+    except:
+        selected3 = ''
+
+    return render_to_response('subject.html', {
+        'level1_list': subject_list,
+        'level2_list': level2_list,
+        'level3_list': level3_list,
+        'selected1': selected1,
+        'selected2': selected2,
+        'selected3': selected3,
         })
