dffc02b71ff226a4bbda13562adb85db5d7b2cf3
j129008 <j129008@gmail.com>
2016-06-30 00:26:53 +0800

    add button and init download function

--- dwn/views.py
+++ dwn/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render_to_response
 from sub.models import subject
+from django.http import HttpResponse
+import csv
+# import os
+# from xlrd import open_workbook
 
 def dwn(request):
     try:
         if 'clear' in request.GET:
             request.session['saved'] = []
+        if 'download' in request.GET:
+            # filePath = './dwn/J1student/'
+            response = HttpResponse(content_type='text/csv')
+            response['Content-Disposition'] = 'attachment; filename="data.csv"'
+            writer = csv.writer(response)
+            for tag in request.GET['download'].split('_'):
+                # for filename in os.listdir(filePath):
+                    # book = open_workbook(filePath+filename)
+                    # first_sheet = book.sheet_by_index(0)
+                writer.writerow(["fuckyou!"])
+                return response
+
     except:
         pass
     tags = {}
@@ -18,7 +34,7 @@ def dwn(request):
         except:
             tags[pk] = []
             tags[pk].append(tag)
-        data[pk] = [rec[0], rec[1], rec[2], subject.objects.get(pk=pk).question ,str(tags[pk])]
+        data[pk] = [rec[0], rec[1], rec[2], subject.objects.get(pk=pk).question ,"_".join(tags[pk])]
     for key in data:
         out.append(data[key])
 
