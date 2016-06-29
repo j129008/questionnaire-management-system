1283610f8ec6bf52aadc11c3a083342b7b48aaad
j129008 <j129008@gmail.com>
2016-06-29 16:20:42 +0800

    finish tag collectin

--- dwn/views.py
+++ dwn/views.py
@@ -6,6 +6,21 @@ def dwn(request):
             request.session['saved'] = []
     except:
         pass
+    tags = {}
+    data = {}
+    out = []
+    for rec in request.session['saved']:
+        pk = rec[3].split('_')[0]
+        tag = rec[3].split('_')[1]
+        try:
+            tags[pk].append(tag)
+        except:
+            tags[pk] = []
+            tags[pk].append(tag)
+        data[pk] = [rec[0], rec[1], rec[2],str(tags[pk])]
+    for key in data:
+        out.append(data[key])
+
     return render_to_response('download.html',{
-        'saved': request.session['saved'],
+        'saved': out,
     })
