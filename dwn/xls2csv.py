7f91caf9bfb474c2040ac7a543a21128d2aae6a3
j129008 <j129008@gmail.com>
2016-06-30 21:26:26 +0800

    add xls to csv converter and do transform

--- dwn/xls2csv.py
+++ dwn/xls2csv.py
@@ -1,25 +1,23 @@
 from xlrd import open_workbook
-book = open_workbook('./J1data.xls')
-first_sheet = book.sheet_by_index(0)
+from glob import glob
+import csv
 
+fileList = glob('J1student/*')
 
-data = []
-level1 = ''
-level2 = ''
-level3 = ''
-for rrange in range(first_sheet.nrows):
-    line = []
-    for ele in first_sheet.row(rrange):
-        cell = ele.value.strip()
-        cell = cell.replace(',', '，').replace('\n','')
-        line.append(cell)
-    if len(line[0])>0:
-        level1 = line[0]
-    if len(line[1])>0:
-        level2 = line[1]
-    if len(line[2])>0:
-        level3 = line[2]
-    out = ''
-    for ele in (['J1'+level1, level2, level3] + line[3:]):
-        out+=ele+','
-    print(out[:-1])
+for fileName in fileList:
+    print(fileName)
+    out = []
+    book = open_workbook(fileName)
+    first_sheet = book.sheet_by_index(0)
+    f = open(fileName.split('.')[0]+'.csv', 'w')
+    w = csv.writer(f)
+
+    for rrange in range(first_sheet.nrows):
+        line = []
+        for ele in first_sheet.row(rrange):
+            cell = ele.value
+            line.append(cell)
+        out.append(line)
+    out = zip(*out)
+    w.writerows(out)
+    f.close()
