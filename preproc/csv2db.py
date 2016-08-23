51416b62d505b24bd24b63bdd2af7808d922f4ec
j129008 <j129008@gmail.com>
2016-08-23 17:53:23 +0800

    remove dup space (include special space <81>)

--- preproc/csv2db.py
+++ preproc/csv2db.py
@@ -1,25 +1,20 @@
+import csv
+import re
 fp = open('./data.csv','r')
-data = []
 
-for line in fp:
-    data.append(line.strip().split(','))
-
-col1=''
-col2=''
-col3=''
-col4=''
-
-db = []
-for line in data:
+for line in csv.reader(fp):
+    for i in range(len(line)):
+        line[i] = re.sub(r'',' ',line[i])
+        line[i] = re.sub('\s+',' ',line[i])
     if len(line[0]) > 0:
-        col1 = line[0].strip()
+        col1 = line[0]
     if len(line[1]) > 0:
-        col2 = line[1].strip()
+        col2 = line[1]
     if len(line[2]) > 0:
-        col3 = line[2].strip()
+        col3 = line[2]
     if len(line[3]) > 0:
-        col4 = line[3].strip()
+        col4 = line[3]
 
     if len(col4)>0 and len(col3)>0:
-        # print(col1+'|'+col2+'|'+col3+'|'+col4)
-        db.append([col1,col2,col3,col4])
+        print(col1+'|'+col2+'|'+col3+'|'+col4)
+        #  db.append([col1,col2,col3,col4])
