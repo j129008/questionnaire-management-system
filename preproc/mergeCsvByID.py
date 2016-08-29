29172e0dc2cae04559b242714c3446ca047b2360
j129008 <j129008@gmail.com>
2016-08-29 22:13:37 +0800

    id1 -> id2

--- preproc/mergeCsvByID.py
+++ preproc/mergeCsvByID.py
@@ -1,7 +1,13 @@
 import csv
 from pprint import pprint
 from glob import glob
-fileList = glob('J1student/*.csv')
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: ./mergeCsvByID.py [folder name]")
+    sys.exit()
+
+fileList = glob(sys.argv[1]+'/*.csv*')
 
 data = dict()
 keyList = list()
@@ -13,24 +19,24 @@ for fileName in fileList:
             if key not in keyList:
                 keyList.append(key)
             try:
-                data[row['id1']][key] = row[key]
+                data[row['id2']][key] = row[key]
             except:
-                data[row['id1']] = dict()
-                data[row['id1']][key] = row[key]
+                data[row['id2']] = dict()
+                data[row['id2']][key] = row[key]
     f.close()
 
 f = open('./merge.csv', 'w')
 w = csv.writer(f, lineterminator='\n')
 
-keyList.remove('id1')
-keyList = ['id1'] + keyList
+keyList.remove('id2')
+keyList = ['id2'] + keyList
 out = [keyList]
 
-for id1 in data: 
-    line = [id1]
+for id2 in data: 
+    line = [id2]
     for ele in keyList[1:]:
         try:
-            line.append(data[id1][ele])
+            line.append(data[id2][ele])
         except:
             line.append('')
     out.append(line)
