469f23d2a9661c5dd61bd3e2175ec67263b65888
j129008 <j129008@gmail.com>
2016-08-29 20:21:43 +0800

    add question_top column

--- sub/models.py
+++ sub/models.py
@@ -5,6 +5,7 @@ class subject(models.Model):
     level2 = models.CharField(max_length=100)
     level3 = models.CharField(max_length=100)
     question = models.CharField(max_length=100)
+    question_top = models.CharField(max_length=100)
     wave = models.CharField(max_length=100)
     def __str__(self):
         return self.level1+' | '+self.level2+' | ' +self.level3+' | '+self.question
