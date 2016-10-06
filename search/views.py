770826dcf9dbf1826f89a43b0d588d913431cebb
j129008 <j129008@gmail.com>
2016-10-06 19:14:19 +0800

    search question complete

--- search/views.py
+++ search/views.py
@@ -5,7 +5,13 @@ def search(request):
     if 'keyword' not in request.GET:
         return render_to_response('search.html', { 'result': '' })
     keyword = request.GET['keyword']
-    pool = [ { 'lv2': str(ele.level2), 'lv3': str(ele.level3), 'question': str(ele.question), 'question_top': str(ele.question_top), 'wave': ele.wave.split(','), 'pk': ele.pk } for ele in subject.objects.filter(question__contains=keyword) ]
+    setPool = [ ele.question_top for ele in subject.objects.filter(question__contains=keyword) ]
+    setPool = list(set(setPool))
+    setPool.remove('')
+    pool = []
+    for top in setPool:
+        pool += [ { 'lv2': str(ele.level2), 'lv3': str(ele.level3), 'question': str(ele.question), 'question_top': str(ele.question_top), 'wave': ele.wave.split(','), 'pk': ele.pk } for ele in subject.objects.filter(question_top=top) ]
+    pool += [ { 'lv2': str(ele.level2), 'lv3': str(ele.level3), 'question': str(ele.question), 'question_top': str(ele.question_top), 'wave': ele.wave.split(','), 'pk': ele.pk } for ele in subject.objects.filter(question_top='', question__contains=keyword) ]
     for ele in pool:
         if ele['question'] == ele['question_top']:
             pool[pool.index(ele)]['sort'] = ele['question_top']
