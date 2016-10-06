159f1fc1ee5f716732efe77e6bce78fee1dd40ed
j129008 <j129008@gmail.com>
2016-10-07 03:01:00 +0800

    add keyword color

--- search/views.py
+++ search/views.py
@@ -10,8 +10,8 @@ def search(request):
     setPool.remove('')
     pool = []
     for top in setPool:
-        pool += [ { 'lv2': str(ele.level2), 'lv3': str(ele.level3), 'question': str(ele.question), 'question_top': str(ele.question_top), 'wave': ele.wave.split(','), 'pk': ele.pk } for ele in subject.objects.filter(question_top=top) ]
-    pool += [ { 'lv2': str(ele.level2), 'lv3': str(ele.level3), 'question': str(ele.question), 'question_top': str(ele.question_top), 'wave': ele.wave.split(','), 'pk': ele.pk } for ele in subject.objects.filter(question_top='', question__contains=keyword) ]
+        pool += [ { 'lv2': str(ele.level2), 'lv3': str(ele.level3), 'question': str(ele.question).replace(keyword, '<strong>'+keyword+'</strong>'), 'question_top': str(ele.question_top).replace(keyword, '<strong>'+keyword+'</strong>'), 'wave': ele.wave.split(','), 'pk': ele.pk } for ele in subject.objects.filter(question_top=top) ]
+    pool += [ { 'lv2': str(ele.level2), 'lv3': str(ele.level3), 'question': str(ele.question).replace(keyword, '<strong>'+keyword+'</strong>'), 'question_top': str(ele.question_top).replace(keyword, '<strong>'+keyword+'</strong>'), 'wave': ele.wave.split(','), 'pk': ele.pk } for ele in subject.objects.filter(question_top='', question__contains=keyword) ]
     for ele in pool:
         if ele['question'] == ele['question_top']:
             pool[pool.index(ele)]['sort'] = ele['question_top']
