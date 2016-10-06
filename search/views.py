10d833c8739219ebcbb409d57090f74d47a5bf23
j129008 <j129008@gmail.com>
2016-10-06 18:50:09 +0800

    finish search

--- search/views.py
+++ search/views.py
@@ -5,8 +5,15 @@ def search(request):
     if 'keyword' not in request.GET:
         return render_to_response('search.html', { 'result': '' })
     keyword = request.GET['keyword']
-    pool = [ { 'question': str(ele.question), 'question_top': str(ele.question_top), 'wave': ele.wave.split(','), 'pk': ele.pk } for ele in subject.objects.filter(question__contains=keyword) ]
+    pool = [ { 'lv2': str(ele.level2), 'lv3': str(ele.level3), 'question': str(ele.question), 'question_top': str(ele.question_top), 'wave': ele.wave.split(','), 'pk': ele.pk } for ele in subject.objects.filter(question__contains=keyword) ]
+    for ele in pool:
+        if ele['question'] == ele['question_top']:
+            pool[pool.index(ele)]['sort'] = ele['question_top']
+        else:
+            pool[pool.index(ele)]['sort'] = ele['question_top'] + ele['question']
+    output_list = sorted( pool, key= lambda x: str(x['sort']) )
 
     return render_to_response('search.html', {
-        'result' : pool
+        'waveCnt': [ 'w'+str(cnt) for cnt in range(1,10)],
+        'output' : output_list
     })
