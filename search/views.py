adb5f6449270076555149cd11c30621a8ec98721
j129008 <j129008@gmail.com>
2016-10-06 18:25:46 +0800

    search by keyword

--- search/views.py
+++ search/views.py
@@ -5,7 +5,8 @@ def search(request):
     if 'keyword' not in request.GET:
         return render_to_response('search.html', { 'result': '' })
     keyword = request.GET['keyword']
+    pool = [ { 'question': str(ele.question), 'question_top': str(ele.question_top), 'wave': ele.wave.split(','), 'pk': ele.pk } for ele in subject.objects.filter(question__contains=keyword) ]
 
     return render_to_response('search.html', {
-        'result' : keyword
+        'result' : pool
     })
