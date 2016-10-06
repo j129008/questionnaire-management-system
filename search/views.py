35e7e0d63408fdae277083c93c3f208ad3245d3e
j129008 <j129008@gmail.com>
2016-10-06 18:07:52 +0800

    response keyword

--- search/views.py
+++ search/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render_to_response
-
-# Create your views here.
+from sub.models import subject
 
 def search(request):
-    return render_to_response('search.html')
+    if 'keyword' not in request.GET:
+        return render_to_response('search.html', { 'result': '' })
+    keyword = request.GET['keyword']
+
+    return render_to_response('search.html', {
+        'result' : keyword
+    })
